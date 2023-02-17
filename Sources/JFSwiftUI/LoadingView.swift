7504a08ea6d3af36822437ec82614fcc44eead6d
Jonas Frey <dev@jonasfrey.de>
2023-02-17 17:07:58 +0100

    Removed unused views

--- Sources/JFSwiftUI/LoadingView.swift
+++ Sources/JFSwiftUI/LoadingView.swift
@@ -6,41 +6,6 @@
 //
 import SwiftUI
 #if !os(macOS)
-
-/// Shows a loading screen while a given condition is met
-@available(iOS 13.0, tvOS 13.0, watchOS 6.0, *)
-public struct LoadingScreen<Content: View>: View {
-    
-    public var isLoading: Bool
-    public var content: Content
-    
-    public init(isLoading: Bool, content: () -> Content) {
-        self.isLoading = isLoading
-        self.content = content()
-    }
-    
-    public var body: some View {
-        // Use a group to erase the type
-        Group {
-            if (isLoading) {
-                // Create a loading screen
-                self.loadingScreen
-            } else {
-                // Show the actual content
-                self.content
-            }
-        }
-    }
-    
-    private var loadingScreen: some View {
-        HStack {
-            ActivityIndicator(style: .medium)
-            Text("Loading...")
-                .font(.headline)
-        }
-    }
-}
-
 /// Shows a loading screen while a given condition is met
 @available(iOS 14.0, tvOS 14.0, watchOS 7.0, *)
 public struct LoadingView<Content>: View where Content: View {
